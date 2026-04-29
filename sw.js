fa46799edf4a8fe04f294013c5a4029399043b86
stimsproductions91-eng <stimsproductions91@gmail.com>
2026-04-29 18:14:45 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Installed');
-});
-
-self.addEventListener('fetch', (e) => {
-    // We leave this mostly blank because WebRTC needs a live internet connection anyway.
-});
